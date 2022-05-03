f4e646f01405f19763a1e56e8982f5af3edd1c02
GowthamKumar06 <gowthamkumarkkp@gmail.com.com>
2022-05-03 20:25:23 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.css";
+
+// Page
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Home />
     </>
   );
 }
